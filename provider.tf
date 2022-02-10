a2faed2b7d79e82cbd33ac9c5a33a9afe59425b5
KennedyUC <chukwukakennedyuche@gmail.com>
2022-02-10 08:18:50 -0800

    apply run

--- provider.tf
+++ provider.tf
@@ -1,4 +1,15 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name     = "k8s_test"
+    resource_group_location = "eastus"
+    storage_account_name    = "k8stfbackend"
+    container_name          = "tfstate"
+    key                     = "dev.terraform.tfstate"
+  }
+}
+
+
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -6,6 +17,7 @@ terraform {
     }
   }
 }
+
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
